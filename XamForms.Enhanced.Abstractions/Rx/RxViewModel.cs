5a6f4d0b400c9440a6e4e6b8c64e6c3be2ddbf82
Łukasz Ławicki <lukasz.lawicki@opstalent.it>
2018-04-05 14:10:16 +0200

    Init RxBaseViewModel

---
+++ XamForms.Enhanced.Abstractions/Rx/RxViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Restauranter.Shared.Extensions;
+using XamForms.Enhanced.ViewModels;
+
+namespace XamForms.Enhanced.Rx
+{
+    public abstract class RxBaseViewModel : BaseViewModel
+    {
+        #region Fields
+
+        protected IList<IDisposable> DisposeBag;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor(s)
+
+        protected RxBaseViewModel()
+        {
+            DisposeBag = new List<IDisposable>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public virtual void ViewAppearing()
+        {
+         
+        }
+
+        public virtual void ViewDisappearing()
+        {
+            DisposeBag?.Dispose();
+        }
+
+        protected abstract void Observe();
+
+        #endregion
+    }
+}
