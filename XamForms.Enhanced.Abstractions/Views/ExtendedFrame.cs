0a437a40ad556afc97218e02d3770a56fc92996b
ll <ll>
2018-04-30 14:22:48 +0200

    Added ExtendedFrame

---
+++ XamForms.Enhanced.Abstractions/Views/ExtendedFrame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace XamForms.Enhanced.Views
+{
+    public class ExtendedFrame : Frame
+    {
+        #region Fields
+
+        public static BindableProperty TappedCommandProperty = BindableProperty.Create(
+            propertyName: nameof(TappedCommand),
+            returnType: typeof(ICommand),
+            declaringType: typeof(ExtendedFrame),
+            defaultValue: null);
+
+        public static BindableProperty CommandParameterProperty = BindableProperty.Create(
+            propertyName: nameof(CommandParameter),
+            returnType: typeof(object),
+            declaringType: typeof(ExtendedFrame),
+            defaultValue: null);
+
+        #endregion
+
+        #region Properties
+
+        public ICommand TappedCommand
+        {
+            get { return (ICommand)GetValue(TappedCommandProperty); }
+            set { SetValue(TappedCommandProperty, value); }
+        }
+
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public event EventHandler Tapped;
+
+        #endregion
+
+        #region Constructor(s)
+
+        public ExtendedFrame()
+        {
+            var clickGesture = new TapGestureRecognizer();
+            clickGesture.Tapped += ClickGesture_Tapped;
+            GestureRecognizers.Add(clickGesture);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void ClickGesture_Tapped(object sender, EventArgs e)
+        {
+            Tapped?.Invoke(this, e);
+            TappedCommand?.Execute(CommandParameter);
+        }
+
+        #endregion
+    }
+}
