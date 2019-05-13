14c3bbb3800743d1ecb14b172f84e1037916195d
Łukasz Ławicki <lla@trackman.com>
2019-05-13 18:02:18 +0200

    Push directly to master YOLO

--- XamForms.Enhanced.Abstractions/Extensions/IEnumerableExtensions.cs
+++ XamForms.Enhanced.Abstractions/Extensions/IEnumerableExtensions.cs
@@ -4,21 +4,31 @@ using System.Linq;
 
 namespace XamForms.Enhanced.Extensions
 {
-    public static class IListExtension
+    public static class IEnumerable
     {
         public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
         {
             return new ObservableCollection<T>(source);
         }
-        
+
         public static bool IsNullOrEMpty<T>(this IEnumerable<T> source)
         {
-            if(source==null || !source.Any())
+            if (source == null || !source.Any())
             {
                 return true;
             }
 
             return false;
+        }
+
+        public static IList<T> CopyToList<T>(this IEnumerable<T> source)
+        {
+            if (source.IsNullOrEMpty())
+            {
+                return new List<T>();
+            }
+
+            return new List<T>(source);
         }
     }
 }
