495b073bf6d420ce549661cfd38637bd5372eb79
Łukasz Ławicki <lukasz.lawicki@wp.pl>
2019-06-03 22:35:33 +0200

    Missing file from previous commit

--- XamForms.Enhanced.Abstractions/Extensions/IListExtensions.cs
+++ XamForms.Enhanced.Abstractions/Extensions/IListExtensions.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace XamForms.Enhanced.Extensions
 {
-    public static class ListExtensions
+    public static class IListExtensions
     {
         public static void AddRange<T>(this IList<T> collection, IEnumerable<T> itemsToAdd)
         {
