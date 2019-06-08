f62c64171ef543a9497b53c8b6d887aebe3cc502
Łukasz Ławicki <lukasz.lawicki@wp.pl>
2019-06-08 20:49:25 +0200

    Added ICommandExtensions

---
+++ XamForms.Enhanced.Abstractions/Extensions/ICommandExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace XamForms.Enhanced.Extensions
+{
+    public static class ICommandExtensions
+    {
+        public static void Execute(this ICommand command)
+        {
+            if (command != null && command.CanExecute(null))
+            {
+                command.Execute(null);
+            }
+        }
+    }
+}
+
